533892a9757249b34173abbc90b2c5f529aea157
mkoner <amzid@hotmail.fr>
2024-05-31 13:48:08 -0500

    Created project Lab4

---
+++ Lab4/Lab4PartA/src/main/java/lab/lab4parta/Lab4PartAApplication.java
@@ -0,0 +1,13 @@
+package lab.lab4parta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4PartAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4PartAApplication.class, args);
+    }
+
+}
---
+++ Lab4/Lab4PartA/src/test/java/lab/lab4parta/Lab4PartAApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.lab4parta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4PartAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

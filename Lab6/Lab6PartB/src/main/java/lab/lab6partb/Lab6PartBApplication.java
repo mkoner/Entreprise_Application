b71c6ce8181ce06843d68dea1c3cd40b147ea1fd
mkoner <amzid@hotmail.fr>
2024-06-04 00:35:06 -0500

    Lab6 partB

---
+++ Lab6/Lab6PartB/src/main/java/lab/lab6partb/Lab6PartBApplication.java
@@ -0,0 +1,13 @@
+package lab.lab6partb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6PartBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab6PartBApplication.class, args);
+	}
+
+}

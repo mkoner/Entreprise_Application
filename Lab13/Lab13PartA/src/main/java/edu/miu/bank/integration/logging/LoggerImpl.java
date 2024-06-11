24255b6fb03af715d06e8cf676c24c5d648254eb
mkoner <amzid@hotmail.fr>
2024-06-11 00:36:49 -0500

    Logging into a log file

--- Lab13/Lab13PartA/src/main/java/edu/miu/bank/integration/logging/LoggerImpl.java
+++ Lab13/Lab13PartA/src/main/java/edu/miu/bank/integration/logging/LoggerImpl.java
@@ -1,12 +1,12 @@
 package edu.miu.bank.integration.logging;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoggerImpl implements Logger {
-
 	public void log(String logString) {
-		java.util.logging.Logger.getLogger("BankLogger").info(logString);
+		LoggerFactory.getLogger(LoggerImpl.class).info(logString);
 	}
 
 }

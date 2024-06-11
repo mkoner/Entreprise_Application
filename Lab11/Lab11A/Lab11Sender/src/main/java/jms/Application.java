f7431815a93e53446ef6ec9f4eae245e66967c18
mkoner <amzid@hotmail.fr>
2024-06-10 23:41:51 -0500

    Sending msg using kafka

---
+++ Lab11/Lab11A/Lab11Sender/src/main/java/jms/Application.java
@@ -0,0 +1,36 @@
+package jms;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+
+
+@EnableJms
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	JmsTemplate jmsTemplate;
+
+	public static void main(String[] args)  {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		context.close();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Person person = new Person("Frank", "Brown");
+		//convert person to JSON string
+		ObjectMapper objectMapper = new ObjectMapper();
+		String personAsString = objectMapper.writeValueAsString(person);
+
+		System.out.println("Sending a JMS message:" + personAsString);
+		jmsTemplate.convertAndSend("testQueue",personAsString);
+	}
+
+}

c42d3b5a8a0095add70bb7d621a44a5594fb4300
mkoner <amzid@hotmail.fr>
2024-06-12 13:51:06 -0500

    Lab14

---
+++ Lab14/Lab14PartA/src/main/java/accounts/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package accounts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import accounts.domain.Account;
+
+
+public interface AccountRepository extends JpaRepository<Account, String>{
+   Account findByAccountHolder(String accountHolder);
+
+}

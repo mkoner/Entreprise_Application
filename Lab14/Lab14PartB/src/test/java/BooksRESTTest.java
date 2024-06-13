bf71cca73e1f9948bbc0c5bf433526a3c9500ef5
mkoner <amzid@hotmail.fr>
2024-06-13 06:52:57 -0500

    added RestAssured tests for methods of the BookController

--- Lab14/Lab14PartB/src/test/java/BooksRESTTest.java
+++ Lab14/Lab14PartB/src/test/java/BooksRESTTest.java
@@ -1,6 +1,7 @@
 import books.domain.Book;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -14,10 +15,8 @@ public class BooksRESTTest {
         RestAssured.port = Integer.valueOf(8080);
         RestAssured.baseURI = "http://localhost";
         RestAssured.basePath = "";
-    }
 
-    @Test
-    public void testGetOneBook() {
+
         // add the book to be fetched
         Book book = new Book("878","Book 123", 18.95, "Joe Smith");
         given()
@@ -25,6 +24,18 @@ public class BooksRESTTest {
                 .body(book)
                 .when().post("/books").then()
                 .statusCode(200);
+    }
+
+    @AfterClass
+    public static  void cleanup(){
+        given()
+                .when()
+                .delete("books/878");
+    }
+
+    @Test
+    public void testGetOneBook() {
+
         // test getting the book
         given()
                 .when()
@@ -37,9 +48,24 @@ public class BooksRESTTest {
                 .body("price",equalTo(18.95f))
                 .body("author",equalTo("Joe Smith"));
         //cleanup
+
+    }
+
+    @Test
+    public void updateBook(){
+        Book book = new Book("878","Book 123", 28.95, "Joe Smith");
         given()
-                .when()
-                .delete("books/878");
+                .contentType("application/json")
+                .body(book)
+                .when().put("/books/878").then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON)
+                .and()
+                .body("isbn",equalTo("878"))
+                .body("title",equalTo("Book 123"))
+                .body("price",equalTo(28.95f))
+                .body("author",equalTo("Joe Smith"));
     }
 
 
